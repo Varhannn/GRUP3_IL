6c80e36385beab45f0bd3307ad6d98aec32c777a
Varhannn <farhannetwork@protonmail.com>
2024-06-20 14:37:24 +0700

    Add Cutscene Stage 2

--- GRUP3_IL/Assets/Scripts/TriggerCutscene/TriggerCutscene2.cs
+++ GRUP3_IL/Assets/Scripts/TriggerCutscene/TriggerCutscene2.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class TriggerCutscene2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PhotoFragmentsManager photoFragmentsManager;
+    public PlayableDirector playableDirector;
+    public GameObject cutsceneCanvas;
+    public GameObject player;
+    private bool isTrigger = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (photoFragmentsManager.currentPhotoFragments == 6 && !isTrigger)
+        {
+            cutsceneCanvas.SetActive(true);
+            playableDirector.Play();
+            isTrigger = true;
+            player.SetActive(false);
+        }
+
+        if (playableDirector.state == PlayState.Paused)
+        {
+            player.SetActive(true);
+            cutsceneCanvas.SetActive(false);
+        }
     }
 }
